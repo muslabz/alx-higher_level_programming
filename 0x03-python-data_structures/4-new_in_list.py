8a9ea59bb043664ef0c47f2578dcb4decfe640e5
muslabz <ajetunmobimust3@gmail.com>
2022-12-05 14:06:24 -0800

    g

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+def new_in_list(my_list, idx, element):
+    if(my_list is not None):
+         m_list = my_list.copy()
+         if idx < 0 or idx >= len(my_list):
+             return my_list
+         else:
+             m_list[idx] = element
+             return m_list
+
