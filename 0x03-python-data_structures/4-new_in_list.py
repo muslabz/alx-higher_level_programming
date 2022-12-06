1e6f4caa85b135def7785622cb805a94b3f4d08d
muslabz <ajetunmobimust3@gmail.com>
2022-12-06 02:44:38 -0800

    g

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return my_list.copy()
-    new_list = my_list.copy()
-    new_list[idx] = element
-    return new_list
+     """Replace an element at specific index without modifying the whole list"""
+     if idx < 0 or idx > (len(my_list) - 1):
+         return my_list
+
+     copy = [x for x in my_list]
+     copy[idx] = element
+     return copy
 
