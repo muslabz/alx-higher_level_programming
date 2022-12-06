12efebea4b7943e29f3dfd81f8d3fb3119b280a9
muslabz <ajetunmobimust3@gmail.com>
2022-12-06 02:41:26 -0800

    g

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list.copy()
     new_list = my_list.copy()
-    if 0 <= idx and idx < len(my_list):
-        new_list[idx] = element
+    new_list[idx] = element
     return new_list
 
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
-    if my_list is None:
-        return
-    if len(my_list) == 0:
-        return
-    max = my_list[0]
-    for i in my_list:
-        if i > max:
-            max = i
-    return 
+max_integer = __import__('9-max_integer').max_integer
+
+my_list = [1, 90, 2, 13, 34, 5, -13, 3]
+max_value = max_integer(my_list)
+print("Max: {}".format(max_value))
