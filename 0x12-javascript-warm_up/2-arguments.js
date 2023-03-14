b8e723db2dad660b710d818d1363b9f163e044c4
muslabz <ajetunmobimust3@gmail.com>
2023-03-14 03:31:09 -0700

    df

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
-const newVar = process.argv.length;
-if (newVar === 2) {
-  console.log('No argument');
-} else if (newVar === 3) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
-}
+const count = process.argv.length;
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
